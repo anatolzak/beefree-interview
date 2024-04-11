7e935bcdbe51f597ca4774f579e08b8dfb7e13aa
anatolzak <53095479+anatolzak@users.noreply.github.com>
2024-04-11 16:54:23 +0300

    add rewrite to api to avoid cors

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'https://interviews-api.beefreeagro.com';
+const API_BASE_URL = process.env.NODE_ENV === 'production' ? '' : 'https://interviews-api.beefreeagro.com';
 
 export const API_ENDPOINTS = {
   listDrones: `${API_BASE_URL}/api/v1/drones`,
